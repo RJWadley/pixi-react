6be750310e74698eada085c88adc924aaf029c6a
Robbie <robbiethewadley@gmail.com>
2025-03-23 15:57:50 -0600

    basic motion.dev support

---
+++ src/motion/index.ts
@@ -0,0 +1,7 @@
+import { motionCatalogue as pixiMotion } from './motionCatalogue';
+
+export { pixiMotion };
+export default pixiMotion;
+
+export { extendMotion as motionExtend } from './extendMotion';
+export { useExtendMotion } from './useExtendMotion';
